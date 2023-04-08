bccab7dd58e2d988f49ad760d4f3dcb38c0b8c37
Sayantan Bose <75779600+Sayantan19@users.noreply.github.com>
2023-04-08 10:41:18 +0530

    Modified things in the page

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 <ul>
 <li>My first MERN stack project (MongoDB, Express.js, React.js, Node.js)
 <li>Used HTML, Tailwind CSS, Bootstrap.
-<li>Deployed using Microsoft Azure Cloud Web-App Service.
+<li >Deploed using Render</li>
 </ul>
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -10,9 +10,6 @@ export default function NavBar() {
             <header className="container text-black-600 body-font" id="head">
                 <div className="mx-auto flex flex-wrap pt-3 pb-3 flex-col md:flex-row items-center">
                     <a className="flex cname title-font font-medium items-center md:mb-0" href="/">
-                        {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg> */}
                         <span className="text-black-900 text-xl">Bosean Interio</span>
                     </a>
                     <nav className="hidden md:flex mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex-wrap items-center text-base justify-center">
@@ -23,7 +20,7 @@ export default function NavBar() {
                     </nav>
 
                     <nav className="hidden md:flex ml-auto md:mr-1 md:py-1 md:pl-2 md:border-l md:border-gray-400 flex-wrap items-center text-base justify-center">
-                        <a href="http://instagram.com"><img className=" mx-0.5 Social" src={insta} alt="Insta" srcSet="" /></a>
+                        <a href="https://instagram.com/boseaninterio?igshid=YmMyMTA2M2Y="><img className=" mx-0.5 Social" src={insta} alt="Insta" srcSet="" /></a>
                         <a href="http://facebook.com"><img className=" mx-0.5 Social" src={fb} alt="FB" srcSet="" /></a>
                         <a href="tel:8777603510"><img className=" mx-0.5 Social" src={call} alt="call" srcSet="" /></a>
                     </nav>
