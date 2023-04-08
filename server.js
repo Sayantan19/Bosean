60eb59170495920a4653257943cc67d8029bced9
Sayantan Bose <75779600+Sayantan19@users.noreply.github.com>
2023-04-08 11:10:33 +0530

    CORS

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(express.json());
 // app.use(mongoose);
